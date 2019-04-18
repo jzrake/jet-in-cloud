105ca6b4e3ec3575124a9f1cb8de128b1057b50b
Jonathan Zrake <jonathan.zrake@gmail.com>
2019-04-18 17:30:43 -0400

    Improve plotting of shock_geometry

--- tools/shock_geometry.py
+++ tools/shock_geometry.py
@@ -53,32 +53,44 @@ def load_or_make_data_product(fname):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("filename")
+parser.add_argument("filenames", nargs='+')
 parser.add_argument("--max-theta-index", default=64, type=int)
 parser.add_argument("--show", action='store_true')
 args = parser.parse_args()
 
-p = load_or_make_data_product(args.filename)
+fig = plt.figure(figsize=[6,8])
+ax1 = fig.add_subplot(2, 1, 1)
+ax2 = fig.add_subplot(2, 1, 2)
+
+colors = [(c * 0.8, c, c * 0.8) for c in np.linspace(0.6, 0.3, len(args.filenames))]
+
+for c, fname in zip(colors, args.filenames):
+	p = load_or_make_data_product(fname)
+
+	if args.show:
+		theta_indexes = p['theta_indexes']
+		shock_radial_indexes = p['shock_radial_indexes']
+		shock_radii = p['shock_radii']
+		flow_luminosities_at_shock = p['flow_luminosities_at_shock']
+		thetas = p['thetas']
+		Lengine = p['Lengine']
+
+		label1 = r'$r_{\rm shock}$' if fname == args.filenames[0] else None
+		label2 = r'$L(\theta, r_{\rm inner})$' if fname == args.filenames[0] else None
+
+		ax1.plot(thetas, shock_radii, lw=3, c=c, label=label1)
+		ax2.plot(thetas, flow_luminosities_at_shock, lw=3, c=c, label=r'$L(\theta, r_{\rm shock})$')
+
+		if fname == args.filenames[-1]:
+			ax2.plot(thetas, Lengine, label=label2, ls='--', lw=1, c='k')
+
 
 if args.show:
-	theta_indexes = p['theta_indexes']
-	shock_radial_indexes = p['shock_radial_indexes']
-	shock_radii = p['shock_radii']
-	flow_luminosities_at_shock = p['flow_luminosities_at_shock']
-	thetas = p['thetas']
-	Lengine = p['Lengine']
-
-	fig = plt.figure(figsize=[6,8])
-	ax1 = fig.add_subplot(2, 1, 1)
-	ax2 = fig.add_subplot(2, 1, 2)
-	ax1.plot(thetas, shock_radii, label=r'$r_{\rm shock}$')
-	ax2.plot(thetas, flow_luminosities_at_shock, label=r'$L(\theta, r_{\rm shock})$')
-	ax2.plot(thetas, Lengine, label=r'$L(\theta, r_{\rm inner})$')
 	ax1.legend()
 	ax2.legend()
 	ax1.set_ylabel('radius (cm)')
 	ax2.set_yscale('log')
 	ax2.set_xlabel(r'$\theta$')
-	ax2.set_ylim(1e-5 * min(flow_luminosities_at_shock), 2 * max(flow_luminosities_at_shock))
+	ax2.set_ylim(1e-1 * min(flow_luminosities_at_shock), 2 * max(flow_luminosities_at_shock))
 	ax2.set_ylabel(r'Luminosity (erg/s/Sr)')
 	plt.show()
